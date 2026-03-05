1aa6f9e4cfd59b3b26f1edda060b67cab10d6dbc
jjoanneporter <jporter9@hawk.iit.edu>
2026-03-04 21:20:02 -0600

    Exercises 2 and 3

--- jporter-lab3.js
+++ jporter-lab3.js
@@ -21,7 +21,44 @@ function minMaxAverage(arr) {
     console.log("Average:", average);
 }
 
-// TEST CASES 
+// TEST CASES for Exercise 1
 minMaxAverage([2,5,23,6,9,4,30,1]);
 minMaxAverage([1,55,13,15,13,12,18,16]);
 minMaxAverage([15,40,30,45,55]);
+
+
+// Exercise 2
+function countVowels(str) {
+
+    let vowels = "aeiou";
+    let count = 0;
+
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    console.log(str + ": " + count + " vowels");
+}
+
+// Test cases for Exercise 2
+countVowels("Happy");
+countVowels("Bumblebee");
+countVowels("Soccer");
+
+// Exercise 3
+function sortNumbers(arr) {
+
+    let sorted = [...arr].sort(function(a, b) {
+        return a - b;
+    });
+
+    console.log("Original Array:", arr);
+    console.log("Sorted Array:", sorted);
+}
+
+// Test cases for Exercise 3
+sortNumbers([9, 4, 6, 2]);
+sortNumbers([10, 3, 8, 1]);
+sortNumbers([20, 15, 5, 30]);
